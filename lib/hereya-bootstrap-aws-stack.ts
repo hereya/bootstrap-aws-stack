92abf5bc1e42f45ae1ff6f3fbd458456d49e5565
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-05-23 18:42:11 +0200

    Add CodeBuild project into the AWS stack
    Added a new CodeBuild project named 'hereyaCdk' into the HereyaBootstrapAwsStack class. This project utilises Linux defined environment variables, installation phase, and build phase. Additionally, administrator access was granted to the project's role and an output was added for the project's name.

--- lib/hereya-bootstrap-aws-stack.ts
+++ lib/hereya-bootstrap-aws-stack.ts
@@ -1,16 +1,62 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput } from 'aws-cdk-lib';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class HereyaBootstrapAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const project = new codebuild.Project(this, 'hereyaCdk', {
+      projectName: 'hereyaCdk',
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        env: {
+          shell: 'bash',
+          variables: {
+            HEREYA_ID: '',
+            HEREYA_IAC_TYPE: '',
+            HEREYA_DESTROY: '',
+            HEREYA_INFRA_TYPE: '',
+            HEREYA_PARAMETERS: '',
+            HEREYA_WORKSPACE_ENV: '',
+            PKG_REPO_URL: '',
+          }
+        },
+        phases: {
+          install: {
+            'runtime-versions': {
+              nodejs: '20.x',
+            },
+            commands: [
+              'npm install -g hereya-cli',
+            ],
+          },
+          pre_build: {
+            commands: [
+              'git clone $PKG_REPO_URL source-code/',
+            ],
+          },
+          build: {
+            commands: [
+              'cd source-code',
+              'npm install',
+              'hereya remote exec $PWD',
+            ],
+          },
+        },
+      }),
+    });
+    // Add permissions to the project's role
+    project.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HereyaBootstrapAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'cdkCodebuildProjectName', {
+      value: project.projectName,
+      description: 'The name of the CodeBuild project for CDK deployment',
+    })
   }
 }
