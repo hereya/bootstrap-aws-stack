7fe2f968ac119698259040f08bc3ebed50ffe1e6
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-05-25 14:52:45 +0200

    Enhanced AWS CDK stack with S3 for deploy package support
    This update enhances the AWS CDK stack to integrate S3 in the build process of AWS CodeBuild. It specifically includes an S3 bucket for storing Hereya projects source code and modifies the build script to download the source code from S3 before executing the build.

--- lib/hereya-bootstrap-aws-stack.ts
+++ lib/hereya-bootstrap-aws-stack.ts
@@ -1,62 +1,76 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import { CfnOutput } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class HereyaBootstrapAwsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const project = new codebuild.Project(this, 'hereyaCdk', {
-      projectName: 'hereyaCdk',
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
-      },
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        env: {
-          shell: 'bash',
-          variables: {
-            HEREYA_ID: '',
-            HEREYA_IAC_TYPE: '',
-            HEREYA_DESTROY: '',
-            HEREYA_INFRA_TYPE: '',
-            HEREYA_PARAMETERS: '',
-            HEREYA_WORKSPACE_ENV: '',
-            PKG_REPO_URL: '',
-          }
-        },
-        phases: {
-          install: {
-            'runtime-versions': {
-              nodejs: '20.x',
+        const project = new codebuild.Project(this, 'hereyaCdk', {
+            projectName: 'hereyaCdk',
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
             },
-            commands: [
-              'npm install -g hereya-cli',
-            ],
-          },
-          pre_build: {
-            commands: [
-              'git clone $PKG_REPO_URL source-code/',
-            ],
-          },
-          build: {
-            commands: [
-              'cd source-code',
-              'npm install',
-              'hereya remote exec $PWD',
-            ],
-          },
-        },
-      }),
-    });
-    // Add permissions to the project's role
-    project.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+            buildSpec: codebuild.BuildSpec.fromObject({
+                version: '0.2',
+                env: {
+                    shell: 'bash',
+                    variables: {
+                        HEREYA_ID: '',
+                        HEREYA_IAC_TYPE: '',
+                        HEREYA_DESTROY: '',
+                        HEREYA_INFRA_TYPE: '',
+                        HEREYA_PARAMETERS: '',
+                        HEREYA_WORKSPACE_ENV: '',
+                        PKG_REPO_URL: '',
+                        HEREYA_PROJECT_S3_KEY: '',
+                        HEREYA_DEPLOY: '',
+                    }
+                },
+                phases: {
+                    install: {
+                        'runtime-versions': {
+                            nodejs: '20.x',
+                        },
+                        commands: [
+                            'npm install -g hereya-cli',
+                        ],
+                    },
+                    pre_build: {
+                        commands: [
+                            'git clone $PKG_REPO_URL source-code/',
+                            'aws s3 cp s3://hereya-projects-source-code/$HEREYA_PROJECT_S3_KEY project-source-code/ --recursive',
+                        ],
+                    },
+                    build: {
+                        commands: [
+                            'cd source-code',
+                            'npm install',
+                            'hereya remote exec $PWD --source ../project-source-code',
+                        ],
+                    },
+                },
+            }),
+        });
+        // Add permissions to the project's role
+        project.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
 
-    new CfnOutput(this, 'cdkCodebuildProjectName', {
-      value: project.projectName,
-      description: 'The name of the CodeBuild project for CDK deployment',
-    })
-  }
+        // create s3 bucket for storing hereya projects source code
+        const bucket = new s3.Bucket(this, 'hereya-projects-source-code', {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+
+        new CfnOutput(this, 'cdkCodebuildProjectName', {
+            value: project.projectName,
+            description: 'The name of the CodeBuild project for CDK deployment',
+        })
+
+        new CfnOutput(this, 'hereyaSourceCodeBucketName', {
+            value: bucket.bucketName,
+            description: 'The name of the S3 bucket for storing hereya projects source code',
+        })
+    }
 }
