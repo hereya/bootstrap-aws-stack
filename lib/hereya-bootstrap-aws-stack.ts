391111a1be6e7ee159a5a063ea676e5967b93974
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-05-25 16:03:04 +0200

    Add conditional check before S3 copy in pre_build commands
    Updated the AWS CodeBuild pre_build commands in the Hereya Bootstrap script to only copy the project source code from S3 if the `HEREYA_DEPLOY` flag is set to true. This ensures that unnecessary data transfers from cloud storage are avoided when not in deployment mode.

--- lib/hereya-bootstrap-aws-stack.ts
+++ lib/hereya-bootstrap-aws-stack.ts
@@ -42,7 +42,9 @@ export class HereyaBootstrapAwsStack extends cdk.Stack {
                     pre_build: {
                         commands: [
                             'git clone $PKG_REPO_URL source-code/',
-                            'aws s3 cp s3://hereya-projects-source-code/$HEREYA_PROJECT_S3_KEY project-source-code/ --recursive',
+                            'if [[ "$HEREYA_DEPLOY" == "true" ]] ; then ; aws s3 cp' +
+                            ' s3://hereya-projects-source-code/$HEREYA_PROJECT_S3_KEY' +
+                            ' project-source-code/ --recursive; fi',
                         ],
                     },
                     build: {
