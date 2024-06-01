81566e8f8c8e4d469a4b84e0b178544c1cac33b8
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-06-01 23:59:41 +0200

    Update AWS S3 bucket configuration
    Removed the hardcoded bucket name in the S3 bucket configuration. This change will allow AWS to generate a unique bucket name for each deployment. An export name was also added to the S3 bucket name output, improving cloudformation template reusability.

--- lib/hereya-bootstrap-aws-stack.ts
+++ lib/hereya-bootstrap-aws-stack.ts
@@ -63,7 +63,6 @@ export class HereyaBootstrapAwsStack extends cdk.Stack {
         // create s3 bucket for storing hereya projects source code
         const bucket = new s3.Bucket(this, 'hereya-projects-source-code', {
             removalPolicy: cdk.RemovalPolicy.DESTROY,
-            bucketName: 'hereya-projects-source-code',
         });
 
         new CfnOutput(this, 'cdkCodebuildProjectName', {
@@ -74,6 +73,7 @@ export class HereyaBootstrapAwsStack extends cdk.Stack {
         new CfnOutput(this, 'hereyaSourceCodeBucketName', {
             value: bucket.bucketName,
             description: 'The name of the S3 bucket for storing hereya projects source code',
+            exportName: 'hereyaSourceCodeBucketName',
         })
     }
 }
