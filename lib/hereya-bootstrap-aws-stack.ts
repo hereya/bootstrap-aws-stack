37bd97bc6434e091eeb76f2efca179a7292678f4
Jonatan Sombié <jonatan.sombie@gmail.com>
2025-01-17 23:52:16 +0100

    remove unnecessary codebuild project

--- lib/hereya-bootstrap-aws-stack.ts
+++ lib/hereya-bootstrap-aws-stack.ts
@@ -10,63 +10,6 @@ export class HereyaBootstrapAwsStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // create s3 bucket for storing hereya projects source code
-        const bucket = new s3.Bucket(this, 'hereya-projects-source-code', {
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-            autoDeleteObjects: true,
-        });
-
-        const project = new codebuild.Project(this, 'hereyaCdk', {
-            projectName: 'hereyaCdk',
-            environment: {
-                buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
-            },
-            buildSpec: codebuild.BuildSpec.fromObject({
-                version: '0.2',
-                env: {
-                    shell: 'bash',
-                    variables: {
-                        HEREYA_ID: '',
-                        HEREYA_IAC_TYPE: '',
-                        HEREYA_DESTROY: '',
-                        HEREYA_INFRA_TYPE: '',
-                        HEREYA_PARAMETERS: '',
-                        HEREYA_WORKSPACE_ENV: '',
-                        PKG_REPO_URL: '',
-                        HEREYA_PROJECT_S3_KEY: '',
-                        HEREYA_DEPLOY: '',
-                        HEREYA_SOURCE_CODE_BUCKET: bucket.bucketName,
-                    }
-                },
-                phases: {
-                    install: {
-                        'runtime-versions': {
-                            nodejs: '18.x',
-                        },
-                        commands: [
-                            'npm install -g hereya-cli',
-                            'npm install -g aws-cdk',
-                        ],
-                    },
-                    pre_build: {
-                        commands: [
-                            'git clone $PKG_REPO_URL source-code/',
-                            'if [[ "$HEREYA_DEPLOY" == "true" ]] ; then  aws s3 cp s3://$HEREYA_SOURCE_CODE_BUCKET/$HEREYA_PROJECT_S3_KEY project-source-code/ --recursive; fi',
-                        ],
-                    },
-                    build: {
-                        commands: [
-                            'cd source-code',
-                            'npm install',
-                            'if [[ "$HEREYA_DEPLOY" == "true" ]] ; then hereya remote exec $PWD --source ../project-source-code; else hereya remote exec $PWD; fi',
-                        ],
-                    },
-                },
-            }),
-        });
-        // Add permissions to the project's role
-        project.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
-
         const terraformStateBucket = new s3.Bucket(this, 'terraformStateBucket', {
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
             encryption: s3.BucketEncryption.S3_MANAGED,
@@ -82,88 +25,10 @@ export class HereyaBootstrapAwsStack extends cdk.Stack {
             },
         });
 
-        const terraformProject = new codebuild.Project(this, 'hereyaTerraform', {
-            projectName: 'hereyaTerraform',
-            environment: {
-                buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
-            },
-            buildSpec: codebuild.BuildSpec.fromObject({
-                version: '0.2',
-                env: {
-                    shell: 'bash',
-                    variables: {
-                        HEREYA_ID: '',
-                        HEREYA_IAC_TYPE: '',
-                        HEREYA_DESTROY: '',
-                        HEREYA_INFRA_TYPE: '',
-                        HEREYA_PARAMETERS: '',
-                        HEREYA_WORKSPACE_ENV: '',
-                        PKG_REPO_URL: '',
-                        HEREYA_PROJECT_S3_KEY: '',
-                        HEREYA_DEPLOY: '',
-                        HEREYA_SOURCE_CODE_BUCKET: bucket.bucketName,
-                        TERRAFORM_STATE_BUCKET: terraformStateBucket.bucketName,
-                        TERRAFORM_STATE_LOCK_TABLE: tfStateLockTable.tableName,
-                        AWS_REGION: this.region,
-                    }
-                },
-                phases: {
-                    install: {
-                        'runtime-versions': {
-                            nodejs: '18.x',
-                        },
-                        commands: [
-                            'sudo add-apt-repository ppa:cncf-buildpacks/pack-cli',
-                            'sudo apt-get update -y && sudo apt-get install -y pack-cli',
-                            'sudo apt-get update && sudo apt-get install -y gnupg software-properties-common',
-                            'wget -O- https://apt.releases.hashicorp.com/gpg | \\\n' +
-                            'gpg --dearmor | \\\n' +
-                            'sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null\n',
-                            'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \\\n' +
-                            'https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \\\n' +
-                            'sudo tee /etc/apt/sources.list.d/hashicorp.list\n',
-                            'sudo apt update && sudo apt install terraform',
-                            'npm install -g hereya-cli',
-                        ],
-                    },
-                    pre_build: {
-                        commands: [
-                            'git clone $PKG_REPO_URL source-code/',
-                            'if [[ "$HEREYA_DEPLOY" == "true" ]] ; then  aws s3 cp' +
-                            ` s3://$HEREYA_SOURCE_CODE_BUCKET/$HEREYA_PROJECT_S3_KEY` +
-                            ' project-source-code/ --recursive; fi',
-                        ],
-                    },
-                    build: {
-                        commands: [
-                            'cd source-code',
-                            'echo "terraform {\n' +
-                            '  backend \\"s3\\" {\n' +
-                            '    bucket = \\"$TERRAFORM_STATE_BUCKET\\"\n' +
-                            '    key = \\"$HEREYA_ID/terraform.tfstate\\"\n' +
-                            '    region = \\"$AWS_REGION\\"\n' +
-                            '    dynamodb_table = \\"$TERRAFORM_STATE_LOCK_TABLE\\"\n' +
-                            '  }\n' +
-                            '}" > hereya_terraform_backend.tf\n',
-                            'if [[ "$HEREYA_DEPLOY" == "true" ]] ; then hereya remote exec $PWD --source ../project-source-code; else hereya remote exec $PWD; fi',
-                        ],
-                    },
-                },
-            }),
-        });
-
-        terraformProject.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
-
-
-        new CfnOutput(this, 'cdkCodebuildProjectName', {
-            value: project.projectName,
-            description: 'The name of the CodeBuild project for CDK deployment',
-        })
-
-        new CfnOutput(this, 'hereyaSourceCodeBucketName', {
-            value: bucket.bucketName,
-            description: 'The name of the S3 bucket for storing hereya projects source code',
-            exportName: 'hereyaSourceCodeBucketName',
+        new CfnOutput(this, 'terraformStateBucketRegion', {
+            value: terraformStateBucket.env.region,
+            description: 'The region of the S3 bucket for storing terraform state files',
+            exportName: 'hereyaTerraformStateBucketRegion',
         })
 
         new CfnOutput(this, 'terraformStateBucketName', {
