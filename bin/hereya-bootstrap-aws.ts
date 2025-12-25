d706bfd67597ffa44e0013bd345ad0e57c8b64c4
Jonatan Sombié <jonatan.sombie@gmail.com>
2025-12-25 23:20:12 +0100

    Enable CDK environment configuration for account and region
    Configure the stack to use CDK_DEFAULT_ACCOUNT and CDK_DEFAULT_REGION
    environment variables, allowing region-specific features and lookups.

--- bin/hereya-bootstrap-aws.ts
+++ bin/hereya-bootstrap-aws.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { HereyaBootstrapAwsStack } from '../lib/hereya-bootstrap-aws-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { HereyaBootstrapAwsStack } from "../lib/hereya-bootstrap-aws-stack";
 
 const app = new cdk.App();
 new HereyaBootstrapAwsStack(app, process.env.STACK_NAME!, {
@@ -11,7 +11,10 @@ new HereyaBootstrapAwsStack(app, process.env.STACK_NAME!, {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
